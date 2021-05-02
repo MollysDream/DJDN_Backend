1c0fcdee9ffc665637a2be22efcdee52ada0d913
Kim234 <djehoaos@naver.com>
2021-05-02 16:46:24 +0900

    add certifyAround feature

--- models/address.js
+++ models/address.js
@@ -6,25 +6,29 @@ const address_Schema=mongoose.Schema;
 const addressSchema=new address_Schema({
     isAuth:{
         type:Boolean,
-        required:true
+        default:false
     },
     //넓은범위주소, ex)우만동, 원천동
     addressName:{
         type:String,
         required:true
     },
-    //상세주소
-    detailLocation:{
-        type:String,
-        required:true
-    },
+    // //상세주소
+    // detailLocation:{
+    //     type:String,
+    //     required:true
+    // },
     latitude:{
         type:String,
-        required:true
+        required:false
     },
     longitude:{
         type:String,
-        required:true
+        required:false
+    },
+    email:{
+        type:String,
+        required:false
     }
 })
 
