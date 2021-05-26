745de1012cb84c3bc90e5ded6a1c0867c68956ae
JUNYEOP <junyeoop@gmail.com>
2021-05-27 03:08:40 +0900

    Iamport Web

--- test/categoryTest.js
+++ test/categoryTest.js
@@ -11,7 +11,7 @@ const expect = require('chai').expect;
 mongoose.set('useCreateIndex', true)
 
 
-describe('Drop all collections before each test',()=>{F
+describe('Drop all collections before each test',()=>{
 	it('Before start, drop all collections',(done)=>{
 		// Drop the collection
 		mongoose.connection.collections.categories.drop(()=>{
